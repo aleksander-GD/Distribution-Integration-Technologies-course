e9610294ad97932b230ef2598c304bf988457938
AleksanderGD <agdbusiness@hotmail.com>
2020-11-09 13:22:39 +0100

    lec8 exercises done

--- lec8/SecureTheChat/lec08_chat-server.js
+++ lec8/SecureTheChat/lec08_chat-server.js
@@ -1,13 +1,14 @@
-const {Server} = require('ws');
+const { Server } = require('ws');
+let validator = require('validator');
 let wsserver = new Server({ port: 8080, path: '/' });
 let latest = { time: Date.now(), msg: "no messages yet" };
 
- wsserver.on('connection', ws => {
+wsserver.on('connection', ws => {
     console.log("New client connected");
-    ws.send(JSON.stringify(latest)+"\n");
+    ws.send(JSON.stringify(latest) + "\n");
     ws.on('close', (code, msg) => console.log("Connection closing", code, msg));
     ws.on('message', msg => {
-        latest = { time: Date.now(), msg: msg };
+        latest = { time: Date.now(), msg: validator.escape(msg) };
         console.log("Message arrived", msg);
         wsserver.clients.forEach(c => c.send(JSON.stringify(latest)));
     });
